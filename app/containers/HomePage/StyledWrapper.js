704d883654799d666dc20b7e6d80b2e0a244030b
clfhhc <clfhhc@gmail.com>
2018-08-03 18:34:24 -0700

    Modified title message and added styled wrapper to title

---
+++ app/containers/HomePage/StyledWrapper.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  text-align: center;
+  > h1 {
+    font-size: 40px;
+  }
+`;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,15 +11,18 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import StyledWrapper from './StyledWrapper';
 import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <StyledWrapper>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+      </StyledWrapper>
     );
   }
 }
