bde76ed6b8e22cb740afc0caffd20a8bfb4d901c
clfhhc <clfhhc@gmail.com>
2018-08-07 05:27:09 -0700

    change the test name

--- app/containers/ChangeInputPage/tests/index.test.js
+++ app/containers/ChangeInputPage/tests/index.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { shallow } from 'enzyme';
 
-import InputPage from '../index';
+import ChangeInputPage from '../index';
 import messages from '../messages';
 
-describe('<HomePage />', () => {
+describe('<ChangeInputPage />', () => {
   it('should render the page message', () => {
-    const renderedComponent = shallow(<InputPage />);
+    const renderedComponent = shallow(<ChangeInputPage />);
     expect(
       renderedComponent.contains(<FormattedMessage {...messages.header} />),
     ).toEqual(true);
